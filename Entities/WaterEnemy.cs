e33f29167f18071ef5bf30564043b7d13ab5dd79
koseidiamond <109972200+koseidiamond@users.noreply.github.com>
2023-01-22 19:48:33 +0100

    WaterEnemy fix
    Also, now they go back to their original position if you're not on water!

--- Entities/WaterEnemy.cs
+++ Entities/WaterEnemy.cs
@@ -17,21 +17,22 @@ public class WaterEnemy : Actor
     private float speedX = 50f;
     private float speedY = 50f;
     private bool left = true;
-    private bool down = true;
     private bool dead = false;
     private Collider bounceCollider;
     private float noGravityTimer;
     public Holdable Hold;
     public Solid solid;
     public Vector2 previousPosition = new Vector2(0f, 0f); // this will be used to go back to previous position if somehow it gets out of water
+    public Vector2 originalPosition = new Vector2(0f, 0f); // this will be used to go back to the original position if the player is not inside the water
 
     // Constructor
     public WaterEnemy(EntityData data, Vector2 offset)
     : base(data.Position + offset)
     {
-        base.Collider = new Hitbox(46f, 12f, -22f, -2f);
+        base.Collider = new Hitbox(46f, 12f, -22f, -8f);
         Position = data.Position + offset;
         previousPosition = Position;
+        originalPosition = Position;
         speedX = data.Float("speedX");
         speedY = data.Float("speedY");
         Add(new PlayerCollider(OnPlayer));
@@ -73,35 +74,57 @@ public class WaterEnemy : Actor
 
     private void moveTowardsPlayer (Player player)
     {
-        //Acceleration + moving one pixel at a time
-        if (player != null && SwimCheck())
+        if (player != null)
         {
-            float targetPositionX = Calc.Approach(ExactPosition.X, player.Position.X, speedX * Engine.DeltaTime);
-            float toX = ExactPosition.X;
-            while (toX != targetPositionX)
-            {
-                toX = Calc.Approach(toX, targetPositionX, 2f);
-            }
-            if ((ExactPosition.X - player.Position.X) > 0)
+            if ((ExactPosition.X - player.Position.X) > 0) //This is to flip the sprite
                 left = true;
             else
                 left = false;
-            //Actually move
-            MoveToX(toX);
-            //Same but for the Y coordinates
-            float targetPositionY = Calc.Approach(ExactPosition.Y, player.Position.Y, speedY * Engine.DeltaTime);
-            float toY = ExactPosition.Y;
-            while (toY != targetPositionY)
+            //Acceleration + moving one pixel at a time
+            if (SwimCheck() && playerOnWaterCheck())
             {
-                toY = Calc.Approach(toY, targetPositionY, 1f);
+                float targetPositionX = Calc.Approach(ExactPosition.X, player.Position.X, speedX * Engine.DeltaTime);
+                float toX = ExactPosition.X;
+                while (toX != targetPositionX)
+                {
+                    toX = Calc.Approach(toX, targetPositionX, 2f);
+                }
+                //Actually move
+                MoveToX(toX);
+                //Same but for the Y coordinates
+                float targetPositionY = Calc.Approach(ExactPosition.Y, player.Position.Y, speedY * Engine.DeltaTime);
+                float toY = ExactPosition.Y;
+                while (toY != targetPositionY)
+                {
+                    toY = Calc.Approach(toY, targetPositionY, 1f);
+                }
+                //Actually move
+                MoveToY(toY);
             }
-            if ((ExactPosition.Y - player.Position.Y) > 0)
-                down = true;
-            else
-                down = false;
-            //Actually move
-            MoveToY(toY);
+            else//Same but for going back to the originalPosition if you're outside of water
+            {
+                if ((ExactPosition.X - originalPosition.X) > 0) //This is to flip the sprite
+                    left = true;
+                else
+                    left = false;
+                float backToX = Calc.Approach(ExactPosition.X, originalPosition.X, speedX * Engine.DeltaTime);
+                float goesBackToX = ExactPosition.X;
+                while (goesBackToX != backToX)
+                {
+                    goesBackToX = Calc.Approach(goesBackToX, backToX, 1f);
+                }
+                MoveToX(goesBackToX);
+
+                float backToY = Calc.Approach(ExactPosition.Y, originalPosition.Y, speedY * Engine.DeltaTime);
+                float goesBackToY = ExactPosition.Y;
+                while (goesBackToY != backToY)
+                {
+                    goesBackToY = Calc.Approach(goesBackToY, backToY, 1f);
+                }
+                MoveToY(goesBackToY);
+            }
         }
+
     }
 
     //Draws an outline around the sprite
@@ -143,6 +166,16 @@ public class WaterEnemy : Actor
     private bool SwimCheck()
     {
         if (CollideCheck<Water>(Position + Vector2.UnitY * -8f))
+        {
+            return CollideCheck<Water>(Position);
+        }
+        return false;
+    }
+
+    private bool playerOnWaterCheck()
+    {
+        Player player = Scene.Tracker.GetEntity<Player>(); // Searchs for Madeline inside the room
+        if (CollideCheck<Water>(player.Position + Vector2.UnitY * -8f)) //And checks if she's inside water
         {
             return CollideCheck<Water>(Position);
         }
