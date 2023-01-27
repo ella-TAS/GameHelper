730f1317a887ec7abf78f3bbdae9b24636bced51
tntfalle <86128357+ella-TAS@users.noreply.github.com>
2023-01-27 21:04:47 +0100

    custom key bind? what now? no idea

--- GameHelperModuleSettings.cs
+++ GameHelperModuleSettings.cs
@@ -1,5 +1,10 @@
+using Microsoft.Xna.Framework.Input;
+
 namespace Celeste.Mod.GameHelper;
 
 public class GameHelperModuleSettings : EverestModuleSettings {
-
+    #region keyboard
+    [DefaultButtonBinding(Buttons.LeftShoulder, Keys.G)]
+    public ButtonBinding FlashlightButton { get; set; }
+    #endregion
 }
