8adf8cf6f23d33a0e7e5692694b8114d1d342c91
EllaTAS <86128357+ella-TAS@users.noreply.github.com>
2025-02-17 23:06:04 +0100

    camera entity target controller changes

---
+++ Loenn/entities/camera_entity_target.lua
@@ -0,0 +1,18 @@
+local st = {}
+
+st.name = "GameHelper/CameraEntityTargetController"
+st.depth = 8998
+st.texture = "loenn/GameHelper/controller"
+st.placements = {
+    name = "normal",
+    data = {
+        onlyType = "Celeste.Player",
+        flag = "",
+        lerp = 1.0,
+        offsetX = 0.0,
+        offsetY = 0.0,
+        debug = false
+    }
+}
+
+return st
---
+++ src/Entities/Controllers/CameraEntityTargetController.cs
@@ -0,0 +1,59 @@
+using Celeste.Mod.Entities;
+using Celeste.Mod.GameHelper.Utils;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.GameHelper.Entities.Controllers;
+
+[CustomEntity("GameHelper/CameraEntityTargetTrigger", "GameHelper/CameraEntityTargetController")]
+public class CameraEntityTargetController : Wrapper {
+    private readonly string onlyType, flag;
+    private readonly float lerp;
+    private readonly bool resetOnFalse, debug;
+    private readonly Vector2 offset;
+
+    private Entity target;
+
+    public CameraEntityTargetController(EntityData data, Vector2 levelOffset) : base(data.Position + levelOffset) {
+        onlyType = data.Attr("onlyType");
+        flag = data.Attr("flag");
+        lerp = data.Float("lerp");
+        debug = data.Bool("debug");
+        resetOnFalse = data.Bool("resetOnFalse");
+        offset = new Vector2(data.Float("offsetX"), data.Float("offsetY"));
+        Depth = -20;
+    }
+
+    public override void Update() {
+        base.Update();
+        Player p = SceneAs<Level>().Tracker.GetEntity<Player>();
+        if(p == null) {
+            RemoveSelf();
+            return;
+        }
+        if(Util.GetFlag(flag, Scene, true)) {
+            findTarget();
+            p.CameraAnchor = target.Center - new Vector2(160, 90) + offset;
+            p.CameraAnchorLerp = Vector2.One * lerp;
+        } else if(resetOnFalse) {
+            p.CameraAnchor = p.CameraAnchorLerp = Vector2.Zero;
+        }
+    }
+
+    public override void Awake(Scene scene) {
+        base.Awake(scene);
+        if(debug) LogAllEntities();
+        if(Util.GetFlag(flag, Scene, true)) findTarget();
+    }
+
+    private void findTarget() {
+        if(target != null) return;
+
+        target = FindNearest(Position, onlyType);
+        if(target == null) {
+            ComplainEntityNotFound("Camera Entity Target Trigger");
+        } else if(debug) {
+            Logger.Info("GameHelper", "Camera Entity Target Trigger found entity " + target.GetType());
+        }
+    }
+}
