96bd36d06cd922cb0b787c5f0d83f18fb5308ef7
EllaTAS <86128357+ella-TAS@users.noreply.github.com>
2025-03-01 19:07:30 +0100

    jump rope prototype

---
+++ src/Entities/JumpRope.cs
@@ -0,0 +1,50 @@
+using Celeste.Mod.Entities;
+using Celeste.Mod.GameHelper.Utils;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Celeste.Mod.GameHelper.Entities;
+
+[CustomEntity("GameHelper/JumpRope")]
+public class JumpRope : Entity {
+    private readonly List<RopeSegment> segments = new();
+    private readonly Vector2 endVector;
+
+    public JumpRope(EntityData data, Vector2 levelOffset) : base(data.Position + levelOffset) {
+        Depth = 1;
+        endVector = data.Nodes[0] + levelOffset - Position;
+        if(endVector.X < 0) {
+            // endVector is always positive
+            Position = endVector;
+            endVector *= -1f;
+        }
+    }
+
+    public override void Update() {
+        base.Update();
+        Player p = SceneAs<Level>().Tracker.GetEntity<Player>();
+        float endX = X + endVector.X;
+        if(p != null && X < p.X && p.X < endX && segments.Any(s => s.HasPlayerRider())) {
+            p.LiftSpeed = new Vector2(130, 130);
+            p.LiftSpeedGraceTime = 0.05f;
+            segments.ForEach(s => s.BendByPlayer(1f, Calc.Clamp(p.X, X + 16f, endX - 16f)));
+        }
+    }
+
+    public override void Added(Scene scene) {
+        base.Added(scene);
+
+        Vector2 direction = endVector / endVector.X;
+        Logger.Info("GameHelper", direction.ToString());
+        float end = Position.X + endVector.X;
+
+        // create a segment for every pixel until the end position
+        for(Vector2 current = Position; current.X <= end; current += direction) {
+            RopeSegment newSegment = new(current, X, end);
+            SceneAs<Level>().Add(newSegment);
+            segments.Add(newSegment);
+        }
+    }
+}
---
+++ src/Utils/RopeSegment.cs
@@ -0,0 +1,66 @@
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Celeste.Mod.GameHelper.Utils;
+
+[Tracked]
+public class RopeSegment : JumpThru {
+    private const int CORRECTION_ABOVE = 3;
+    private const int CORRECTION_BELOW = 5;
+    private const int MAX_PLAYER_BEND = 10;
+
+    private readonly float anchor, startX, endX;
+
+    public RopeSegment(Vector2 position, float startX = 0f, float endX = 0f) : base(position.Round(), width: 1, safe: false) {
+        Depth = 1;
+        BlockWaterfalls = false;
+        anchor = position.Y;
+        this.startX = startX;
+        this.endX = endX;
+    }
+
+    public void BendByPlayer(float factor, float playerX) {
+        float endPoint = X < playerX ? startX : endX;
+        float linearFactor = (X - endPoint) / (playerX - endPoint);
+        MoveToY((float) Math.Round(anchor + factor * linearFactor * MAX_PLAYER_BEND));
+    }
+
+    public override void DebugRender(Camera camera) {
+        Draw.Point(Position, Color.Orange);
+        Draw.Point(Position + Height * Vector2.UnitY, Color.Cyan);
+    }
+
+    public static void Hook() {
+        On.Celeste.Player.Update += PlayerUpdate;
+    }
+
+    public static void Unhook() {
+        On.Celeste.Player.Update -= PlayerUpdate;
+    }
+
+    private static void PlayerUpdate(On.Celeste.Player.orig_Update orig, Player p) {
+        orig(p);
+
+        if((p.StateMachine.State == 2 && p.DashDir.Y < 0) || p.CollideCheck<RopeSegment>(p.Position + new Vector2(0, -CORRECTION_BELOW))) {
+            // dashing up or too far below
+            return;
+        }
+
+        List<Entity> bridgeInside = p.CollideAll<RopeSegment>();
+        if(bridgeInside.Count > 0) {
+            float niveau = bridgeInside.MinBy(e => e.Position.Y).Y;
+            p.MoveVExact((int) (niveau - p.Y));
+            p.Speed.Y = 0;
+        } else if(p.StateMachine.State == 0 && p.Speed.Y >= 0) {
+            bridgeInside = p.CollideAll<RopeSegment>(p.Position + new Vector2(0, CORRECTION_ABOVE));
+            if(bridgeInside.Count > 0) {
+                float niveau = bridgeInside.MinBy(e => e.Position.Y).Y;
+                p.MoveVExact((int) (niveau - p.Y));
+                p.Speed.Y = 0;
+            }
+        }
+    }
+}
