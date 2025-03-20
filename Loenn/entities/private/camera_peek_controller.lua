a8728005e0c2802dc7762dc4b8c2375b6f779ff0
EllaTAS <86128357+ella-TAS@users.noreply.github.com>
2025-03-20 21:58:52 +0100

    camera peek controller prototype

---
+++ Loenn/entities/private/camera_peek_controller.lua
@@ -0,0 +1,15 @@
+local cpc = {}
+
+cpc.name = "GameHelper/CameraPeekController"
+cpc.depth = -999999999
+cpc.texture = "loenn/GameHelper/camera_peek_controller"
+cpc.placements = {
+    name = "normal",
+    data = {
+        magnitudeUp = 1.0,
+        magnitudeDown = 1.0,
+        pressDuration = 1.0
+    }
+}
+
+return cpc
